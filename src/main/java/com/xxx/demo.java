919f12067fd5cc67fe8853d8090b381d0a78e2f4
Qinghong Dai <qinghongdaig@gmail.com>
2026-01-06 10:04:26 +1100

    init

--- src/main/java/com/xxx/demo.java
+++ src/main/java/com/xxx/demo.java
@@ -1,13 +1,13 @@
 package com.xxx;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.concurrent.locks.ReentrantLock;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demo {
     public static void main(String[] args){
-        // TODO:
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        driver.close();
     }
 }
