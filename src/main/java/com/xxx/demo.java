03bdc064cdb592dd699b997d631521d4e9d43dcd
Qinghong Dai <qinghongdaig@gmail.com>
2026-01-06 10:17:05 +1100

    webdrivermanager add

--- src/main/java/com/xxx/demo.java
+++ src/main/java/com/xxx/demo.java
@@ -1,13 +1,20 @@
 package com.xxx;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demo {
     public static void main(String[] args){
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        driver.get("https://google.com");
         driver.close();
     }
 }
