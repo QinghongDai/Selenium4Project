1b89d7ff528534399bb105343254fcb6e11ba789
Qinghong Dai <qinghongdaig@gmail.com>
2026-01-06 12:06:37 +1100

    test

--- src/main/java/com/xxx/demo.java
+++ src/main/java/com/xxx/demo.java
@@ -1,13 +1,13 @@
 package com.xxx;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demo {
-    public static void main(String[] args){
-        WebDriverManager.chromedriver().setup();
+    public static void main(String[] args) throws InterruptedException {
+        //WebDriverManager.chromedriver().driverVersion("").setup();
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -15,6 +15,25 @@ public class demo {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
         driver.get("https://google.com");
+        String originalWindow = driver.getWindowHandle();
+
+        driver.navigate().to("https://automationstepbystep.com");
+
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+
+        driver.switchTo().window(originalWindow);
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        Thread.sleep(2000);
         driver.close();
+        driver.quit();
     }
 }
